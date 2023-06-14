30ac8aa1267ed82dbc61578558ab469df5c5c09d
joonmo-you <joonmo.you@gmail.com>
2023-06-15 02:40:25 +0900

    Exercise 2 solution

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ import Logo from "../Logo";
 import MobileMenu from "../MobileMenu";
 import SuperHeader from "../SuperHeader";
 import UnstyledButton from "../UnstyledButton";
+import VisuallyHidden from "../VisuallyHidden/VisuallyHidden";
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -20,29 +21,32 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Side>
+        <LogoWrapper>
           <Logo />
-        </Side>
-        <Nav>
+        </LogoWrapper>
+        <DesktopNav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        </DesktopNav>
         <MobilActions>
-          <UnstyledButton>
+          <ShoppingBagButton>
             <Icon id="shopping-bag" strokeWidth={2} />
-          </UnstyledButton>
+            <VisuallyHidden>Open cart</VisuallyHidden>
+          </ShoppingBagButton>
           <UnstyledButton>
             <Icon id="search" strokeWidth={2} />
+            <VisuallyHidden>Search</VisuallyHidden>
           </UnstyledButton>
           <UnstyledButton onClick={() => setShowMobileMenu(true)}>
-            <Icon id="hamburger" strokeWidth={2} />
+            <Icon id="menu" strokeWidth={2} />
+            <VisuallyHidden>Open menu</VisuallyHidden>
           </UnstyledButton>
         </MobilActions>
+        <Filler />
       </MainHeader>
 
       <MobileMenu isOpen={showMobileMenu} onDismiss={() => setShowMobileMenu(false)} />
@@ -59,11 +63,17 @@ const MainHeader = styled.div`
 
   @media ${QUERIES.tabletAndSmaller} {
     border-top: 4px solid ${COLORS.gray[900]};
-    gap: ;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  @media ${QUERIES.phoneAndSmaller} {
+    padding-left: 16px;
+    padding-right: 16px;
   }
 `;
 
-const Nav = styled.nav`
+const DesktopNav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
@@ -73,8 +83,20 @@ const Nav = styled.nav`
   }
 `;
 
-const Side = styled.div`
+const LogoWrapper = styled.div`
   flex: 1;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    flex: revert;
+  }
+`;
+
+const Filler = styled.div`
+  flex: 1;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
 
 const NavLink = styled.a`
@@ -87,6 +109,10 @@ const NavLink = styled.a`
   &:first-of-type {
     color: ${COLORS.secondary};
   }
+`;
+
+const ShoppingBagButton = styled(UnstyledButton)`
+  transform: translateX(-2px);
 `;
 
 const MobilActions = styled.div`
